c97040b41e556a95ce9dff162c82f0df816b1aca
test <test1>
2018-12-15 12:19:18 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/a20181215_01_helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.tjeit.a20181215_01_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
